0880b55de9182917172547cade726b08dd19d06f
Raxmatilla <wi.fi.xor@gmail.com>
2020-09-10 15:05:47 +0500

    Majmualar uchun Componentlar ochdim

---
+++ plugins/raxmatilla/majmualar/components/MajmualarCompo.php
@@ -0,0 +1,19 @@
+<?php namespace Raxmatilla\Majmualar\Components;
+
+use Cms\Classes\ComponentBase;
+
+class MajmualarCompo extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'MajmualarCompo Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
